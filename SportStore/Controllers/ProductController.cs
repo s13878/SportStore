c31bca6e96b2aaa489e9de00d149c67f0eb5daec
Natsu <Natsu@DESKTOP-63AHI8S>
2018-08-18 01:39:06 +0200

    Utworzenie Kontrolera z metodą List()

---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
