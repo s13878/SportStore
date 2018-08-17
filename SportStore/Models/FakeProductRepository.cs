3ca5425539000a3a9a8cecc21518093486624877
Natsu <Natsu@DESKTOP-63AHI8S>
2018-08-18 01:28:22 +0200

    Stworzenie prostego modelu i Repozytorium

---
+++ SportStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product{ Name = "Piłka Nożna", Price = 25},
+            new Product{ Name = "Deska surfingowa", Price = 179},
+            new Product{ Name = "Buty do biegania", Price = 95}
+        }.AsQueryable<Product>();
+    }
+}
