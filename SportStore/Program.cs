cf697444d397747192e5733b3a65af1044154338
Natsu <Natsu@DESKTOP-63AHI8S>
2018-08-18 13:35:15 +0200

    wyłączenie walidacji zakresu

--- SportStore/Program.cs
+++ SportStore/Program.cs
@@ -20,6 +20,8 @@ namespace SportStore
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false)
                 .Build();
     }
 }
